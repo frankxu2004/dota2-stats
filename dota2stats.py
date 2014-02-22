1b42e37d9b1596a84cec031b3114e082abe0138d
frankxu2004 <frankxu2004@gmail.com>
2014-02-22 13:52:27 +0800

    added abondonment judgement

--- dota2stats.py
+++ dota2stats.py
@@ -18,20 +18,28 @@ def get_account_id():
                 return int(steam_id["response"]["steamid"])
         print "User not found! Try again!"
 
+def get_match_history(account_id64):
+    response = api.get_match_history(account_id=account_id64,date_min=1393046736)
+    if response["result"]["status"] == 1:
+        return response["result"]["matches"]
+    else:
+        print response["result"]["statusDetail"]
+
 account_id64 = get_account_id()
 account_id32 = account_id64%(2**32)
-matches = api.get_match_history(account_id=account_id64)["result"]["matches"]
 heroes_list = api.get_heroes()["result"]["heroes"]
 
+matches = get_match_history(account_id64)
 for match in matches:
     if match["lobby_type"] == 0 or match["lobby_type"] == 7:
         match_id = match["match_id"]
-        match_detail = api.get_match_details(match_id)
-        for player in match["players"]:
-            if player["account_id"] == account_id32:
-                player_hero = get_hero_name(player["hero_id"], heroes_list)
-                if (player["player_slot"]<50 and match_detail["result"]["radiant_win"])\
-                   or (player["player_slot"]>50 and (not match_detail["result"]["radiant_win"])):
+        match_detail = api.get_match_details(match_id)["result"]
+        for player in match_detail["players"]:
+            if player["account_id"]==account_id32 and get_hero_name(player["hero_id"],heroes_list)!=None:
+                player_hero = get_hero_name(player["hero_id"],heroes_list)
+                if ((player["player_slot"]<50 and match_detail["radiant_win"])\
+                   or (player["player_slot"]>50 and (not match_detail["radiant_win"])))\
+                   and (player["leaver_status"]!=2):
                     print str(match_id) + " " + player_hero + " Win"
                 else:
                     print str(match_id) + " " + player_hero + " Loss"
